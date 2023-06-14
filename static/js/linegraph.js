d962416c69513ddb6f8c1a59adc8dd3448d6fde4
Aspen Simpson <aspenliz92@gmail.com>
2023-06-14 17:25:25 -0500

    titles on line graph

--- static/js/linegraph.js
+++ static/js/linegraph.js
@@ -1,7 +1,9 @@
+//call on json data and set up data
 d3.json('resources/output.json').then(function(data) {
   var states = [...new Set(data.map(item => item.STATE))];
   var causes = [...new Set(data.map(item => item.CAUSE_NAME))];
 
+//get dropdown menus working
   var stateDropdown = d3.select('#stateDropdown');
   states.forEach(function(state) {
     stateDropdown.append('option').attr('value', state).text(state);
@@ -18,7 +20,7 @@ d3.json('resources/output.json').then(function(data) {
   // Initialize the chart
   updateChart();
 });
-
+//set up functions
 function updateChart() {
   var selectedState = d3.select('#stateDropdown').property('value');
   var selectedCause = d3.select('#causeDropdown').property('value');
@@ -31,6 +33,7 @@ function updateChart() {
     var years = filteredData.map(item => item.YEAR);
     var deaths = filteredData.map(item => parseInt(item.DEATHS));
 
+    //set up chart container
     var chartContainer = d3.select('#chartContainer');
     chartContainer.html('');
 
@@ -38,6 +41,7 @@ function updateChart() {
       .attr('width', 500)
       .attr('height', 300);
 
+    //set up location of linegraph
     var margin = { top: 20, right: 20, bottom: 30, left: 50 };
     var width = +svg.attr('width') - margin.left - margin.right;
     var height = +svg.attr('height') - margin.top - margin.bottom;
@@ -54,21 +58,43 @@ function updateChart() {
     var xAxis = d3.axisBottom(xScale);
     var yAxis = d3.axisLeft(yScale);
 
+    //set up chart groups and title
     var chartGroup = svg.append('g')
       .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
     chartGroup.append('g')
-      .attr('class', 'x-axis')
-      .attr('transform', 'translate(0,' + height + ')')
-      .call(xAxis);
+    .attr('class', 'x-axis')
+    .attr('transform', 'translate(0,' + height + ')')
+    .call(xAxis);
+
+    chartGroup.append("text")
+    .attr("class", "x-axis-title")
+    .attr("text-anchor", "middle")
+    .attr("x", width / 2)
+    .attr("y", height + margin.bottom)
+    .text("Year");
+
+
 
     chartGroup.append('g')
       .attr('class', 'y-axis')
       .call(yAxis);
 
+
+    chartGroup.append("text")
+    .attr("class", "y label")
+    .attr("text-anchor", "end")
+    .attr("y", 0)
+    .attr("dy", ".90em")
+    .attr("transform","rotate(-90)")
+    .text("Number of Deaths");
+  
+
     var line = d3.line()
       .x(function(d) { return xScale(d.YEAR) + xScale.bandwidth() / 2; })
       .y(function(d) { return yScale(d.DEATHS); });
+
+    
 
     chartGroup.append('path')
       .datum(filteredData)
